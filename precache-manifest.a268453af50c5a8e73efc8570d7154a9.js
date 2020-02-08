b45e060e10b5b5baf6f6ab7d169c020b5dcee866
S1EpoY <slepoy1992@gmail.com>
2020-02-08 09:59:38 +0200

    Updates

---
+++ precache-manifest.a268453af50c5a8e73efc8570d7154a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55f37fbb2c375cb893e56050e0eb12a3",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "c7c22c0ef46c7b03f970",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.80f459c9.chunk.css"
+  },
+  {
+    "revision": "e24fed0f26738ad9ca8b",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.71f01aae.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.71f01aae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7c22c0ef46c7b03f970",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.2566e9a7.chunk.js"
+  },
+  {
+    "revision": "5eea2426daa3643f03a9",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.6819ab10.js"
+  }
+]);
